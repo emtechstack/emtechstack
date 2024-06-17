d3b5ad7fe4a932e4f919eb02c9caa4170eba48a6
erfanvaredi <erfanvaredi@outlook.com>
2024-06-17 14:26:56 +0400

    feat: add e9k

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='emtechstack',
-    version='0.0.2',
+    version='0.0.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -12,6 +12,7 @@ setup(
     entry_points={
         'console_scripts': [
             'emtechstack=emtechstack.cli:cli',
+            'e9k=emtechstack.cli:cli',
         ],
     },
 )
