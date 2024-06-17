a120b428edb33fc0c61cb82b6b346fa16b0ea17c
erfanvaredi <erfanvaredi@outlook.com>
2024-06-17 16:41:37 +0400

    ✅ feat: add black

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='emtechstack',
-    version='0.0.7',
+    version='0.0.9',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'click',
+        'black',
         'requests',
         'tabulate',
         'termcolor',
