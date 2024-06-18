643b80a68e71e413c1a8481be2fafa21bfdd5c91
erfanvaredi <erfanvaredi@outlook.com>
2024-06-18 11:17:04 +0400

    ♻️ trimmed and cleaned

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='emtechstack',
-    version='0.0.34',
+    name="emtechstack",
+    version="0.0.34",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click',
-        'black',
-        'requests',
-        'tabulate',
-        'termcolor',
+        "click",
+        "black",
+        "requests",
+        "tabulate",
+        "termcolor",
     ],
     entry_points={
-        'console_scripts': [
-            'emtechstack=emtechstack.cli:cli',
-            'e9k=emtechstack.cli:cli',
+        "console_scripts": [
+            "emtechstack=emtechstack.cli:cli",
+            "e9k=emtechstack.cli:cli",
         ],
     },
 )
