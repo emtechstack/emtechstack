91cc6c4590e9145da889ce9a7aa6170742dee971
erfanvaredi <erfanvaredi@outlook.com>
2024-06-18 12:57:33 +0400

    chore: Bump up emtechstack package version to 0.0.71

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="emtechstack",
-    version="0.0.69",
+    version="0.0.71",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
