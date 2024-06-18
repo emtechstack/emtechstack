bd81612fb0e8ab7926f2139aa3eba93997a92f59
erfanvaredi <erfanvaredi@outlook.com>
2024-06-18 11:44:19 +0400

    feat: update that to test the upgrade cmd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="emtechstack",
-    version="0.0.41",
+    version="0.0.44",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
