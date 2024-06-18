d4516f60271005f2cc5de34d7f4f9cfe21c41e15
erfanvaredi <erfanvaredi@outlook.com>
2024-06-18 10:16:52 +0400

    🔖 chore: update setup.py version to 0.0.27

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='emtechstack',
-    version='0.0.26',
+    version='0.0.27',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
