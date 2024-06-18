60b48fe001aa4384b192f4f0338a89ec7de483e9
erfanvaredi <erfanvaredi@outlook.com>
2024-06-18 11:42:05 +0400

    feat: update the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="emtechstack",
-    version="0.0.40",
+    version="0.0.41",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
