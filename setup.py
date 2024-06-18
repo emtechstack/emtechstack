d800863df39fc2b1d2e17636069d5181e5c555bc
erfanvaredi <erfanvaredi@outlook.com>
2024-06-18 13:50:36 +0400

    feat: update command list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="emtechstack",
-    version="0.0.82",
+    version="0.0.83",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
