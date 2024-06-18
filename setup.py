74b1242a77d47883330495f7ecbc389afd179336
erfanvaredi <erfanvaredi@outlook.com>
2024-06-18 10:49:02 +0400

    chore: Bump setup.py version to 0.0.33

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='emtechstack',
-    version='0.0.31',
+    version='0.0.33',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
