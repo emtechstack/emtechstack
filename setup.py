e914f446b3e0c194dfdb8f14b352a85cb7b9cb55
erfanvaredi <erfanvaredi@outlook.com>
2024-07-03 12:20:09 +0400

    chore: os independent

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="emtechstack",
-    version="0.0.94",
+    version="0.0.95",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -17,9 +17,16 @@ setup(
             "emtechstack=emtechstack.cli:cli",
             "e9k=emtechstack.cli:cli",
         ],
-        "gui_scripts": [
-            "emtechstack=emtechstack.cli:cli",
-            "e9k=emtechstack.cli:cli",
-        ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
